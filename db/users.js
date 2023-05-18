813cd850402784f298a3bda094452b913a64bf56
jbourg4364 <jasonjbourg22@gmail.com>
2023-05-18 10:55:12 -0500

    finished users db

--- db/users.js
+++ db/users.js
@@ -36,7 +36,6 @@ async function createUser({
     };
 };
 
-
 async function getUserById(userId) {
     try {
         const { rows: [user] } = await client.query(`
@@ -72,10 +71,112 @@ async function updateUser(id, fields = {}) {
     };
 };
 
+async function getUserByUsername(username) {
+    try {
+        const { rows: [user] } = await client.query(`
+        SELECT * FROM users
+        WHERE username = $1;
+        `, [username]);
+
+        return user;
+    } catch (error) {
+        throw error;
+    };
+};
+
+
+async function getUser({ username, password }) {
+    try {
+        const user = await getUserByUsername(username);
+        
+        if(!user) {
+            return null;
+        };
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            return {
+                id: user.id,
+                username: user.username
+            };
+        } else {
+            return null;
+        };
+
+    } catch (error) {
+        throw error;
+    };
+};
+
+async function getUserByFirstAndLastName({ firstName, lastName }) {
+    try {
+        const { rows: [user] } = await client.query(`
+        SELECT * FROM users
+        WHERE firstName = $1 AND lastName = $2
+        `, [firstName, lastName]); 
+
+        return user;
+    } catch (error) {
+        throw error;
+    };
+};
+
+async function getUserByOrderId(orderId) {
+    try {
+        const { rows: [user] } = await client.query(`
+        SELECT "userId" FROM orders
+        WHERE id = $1;
+        `, [orderId]);
+
+        return user;
+
+    } catch (error) {
+        throw error;  
+    }
+};
+
+async function createGuest({ 
+    password,
+    firstName,
+    lastName,
+    phone,
+    email,
+    addressline1,
+    addressline2,
+    isRegistered }) 
+    {
+    try {
+        const { rows: [user] } = await client.query(`
+        WITH new_user AS (
+            INSERT INTO users(username, password, firstName, lastName, phone, email, addressline1, addressline2, "isRegistered")
+            VALUES ($5, $1, $2, $3, $4, $5, $6, $7, $8)
+            ON CONFLICT (username) DO NOTHING
+            RETURNING *
+        )
+        SELECT id, $5 AS username, password
+        FROM new_user;
+        `, [password, firstName, lastName, phone, email, addressline1, addressline2, isRegistered]);
+
+        return {
+            id: user.id,
+            username: user.username,
+            password: null,
+        };
+    } catch (error) {
+        throw error;
+    };
+};
+
 
 
 module.exports = {
     createUser,
     getUserById,
     updateUser,
+    getUserByUsername,
+    getUser,
+    getUserByFirstAndLastName,
+    getUserByOrderId,
+    createGuest
 };
