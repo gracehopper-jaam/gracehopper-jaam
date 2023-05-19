5f2e0f4e7cb05a111eebd6b824d5d41c27e939e2
Aubrey042 <127706254+Aubrey042@users.noreply.github.com>
2023-05-19 11:17:07 -0400

    Routers Added

--- api/index.js
+++ api/index.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/testing", function (req, res, next) {
-  res.send({ message: "bk working fine" });
+/* 
+    Server Status : Here you can check that server is working or not.?
+*/
+router.get("/status", function (req, res, next) {
+  res.send({
+    message: "Welcome to our Grace Shopper e-commerce store server.",
+  });
 });
 
+/* 
+    Router: For Products
+*/
+const productsRouter = require("./products");
+router.use("/products", productsRouter);
+
+/* 
+    Router: For Categories
+*/
+const categoriesRouter = require("./categories");
+router.use("/categories", categoriesRouter);
+
+/* 
+    Router: For orders
+*/
+const ordersRouter = require("./orders");
+router.use("/orders", ordersRouter);
+
+/* 
+    Router middleware 
+*/
 router.use(async (req, res, next) => {
   next();
 });
 
-// ROUTER: // Define all type of router
-
+/* 
+    Router middleware 
+*/
 router.use(function (req, res, next) {
-  res.status(404).send({ message: "Not found" });
+  res.status(404).send({
+    message:
+      "API path not found. Please check if your path and API are correct",
+  });
 });
 
 module.exports = router;
