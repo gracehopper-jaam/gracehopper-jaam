e1e710de2e5566cf935ba14a7c4a3a67dbcb1f65
aparnas12 <127676701+aparnas12@users.noreply.github.com>
2023-05-25 10:08:53 -0400

    Update SingleProductView.jsx
    merge from main to jason

--- client/src/components/SingleProductView.jsx
+++ client/src/components/SingleProductView.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
-import './SingleProductView.css';
-import Images from '../media';
 
 const SingleProductView = (props) => {
 
-const {selectedProduct, setCart} = props;
+const {selectedProduct, setCart,isLoggedIn} = props;
 const [qty, setQty] = useState(1);
 const handleClick = () => {
-
+    const tempUserName = isLoggedIn? localStorage.getItem("currentUser") : "guest";
     let tempCart = JSON.parse(localStorage.getItem("currentCart"));
     if(tempCart)
     {
-            console.log("tempcart exits");
+        console.log("tempcart exits");
+        const tempItem = { id:selectedProduct.id, name: selectedProduct.name, priceperunit: selectedProduct.price, description: selectedProduct.description,qty:qty};
+        let tempCartArr = [...tempCart.items];
+        tempCartArr.push(tempItem);
+        const cartObject = {
+            orderdate:tempCart.orderdate,
+            totalamount:tempCart.totalamount + (qty * selectedProduct.price),
+            items:[...tempCartArr],
+            username: tempUserName,
+            persistedCart : false,
+          }
+          console.log("reached here new cart",cartObject, "compared to old cart",tempCart);
+          localStorage.setItem("currentCart",JSON.stringify(cartObject));
+          setCart(cartObject);
     }
     else
     {
         console.log("tempcart does not exits");
+        const tempItem = { id:selectedProduct.id, name: selectedProduct.name, priceperunit: selectedProduct.price, description: selectedProduct.description,qty:qty};
         const currDate = new Date().toISOString().split('T')[0];
         let tempItems = [];
-        tempItems.push(selectedProduct);
+        tempItems.push(tempItem);
        // create  a new cart object
            const cartObject = {
             orderdate:currDate,
-            totalamount:0,
+            totalamount:qty * selectedProduct.price,
             items:[...tempItems],
-            username: "guest",
+            username: tempUserName,
             persistedCart : false,
           }
           localStorage.setItem("currentCart",JSON.stringify(cartObject));
@@ -39,7 +51,7 @@ return (
           <img
           src={Images[selectedProduct.id]}
           alt={selectedProduct.name}
-          className="product-image"
+          className=“product-image”
           />
           <p className="product-description">{selectedProduct.description}</p>
           <p>{selectedProduct.price}</p>
@@ -50,8 +62,8 @@ return (
           onChange={(event) => setQty(event.target.value)}
           required/>
           <button onClick ={() =>{handleClick(selectedProduct.id)}}>Add To Cart</button>
-          
-      </div>
+          <hr></hr>
+        </div>
     </>
 );
 
