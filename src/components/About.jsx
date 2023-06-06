c69690bd5ed937fe90e93fd6356b44841a0fc396
jbourg4364 <jasonjbourg22@gmail.com>
2023-06-06 10:24:33 -0500

    added css to header and mobile views

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,7 @@ const About = () => {
     <>
     <div id='about'>Hi there!</div>
     <h1 id='welcome'>Learn a bit about us..</h1>
-    <p id='body'> JAAM is today’s principal brand for portable audio technology.  The company was founded in 2023 by Jason, Aubrey, Aparna, and Maisha. They were four unlikely souls who came together with one goal in mind: to spread music to the coding cohorts of the world. The top-tier quality of JAAM’s speakers and headphones astronomically expands the possibilities of sonic entertainment 
-The brand’s commitment to success brings liveliness, passion, and vibrancy to the listening experiences of music lovers worldwide. 
+    <p id='body'>JAAM is today’s principal brand for portable audio technology.  The company was founded in 2023 by Jason, Aubrey, Aparna, and Maisha. They were four unlikely souls who came together with one goal in mind: to spread music to the coding cohorts of the world. The top-tier quality of JAAM’s speakers and headphones astronomically expands the possibilities of sonic entertainment. The brand’s commitment to success brings liveliness, passion, and vibrancy to the listening experiences of music lovers worldwide. 
     </p>
   
     <div id='contact'>  Contact Us  </div>
@@ -21,10 +20,6 @@ The brand’s commitment to success brings liveliness, passion, and vibrancy to 
 
     <button id="submit-button">Submit</button>
     </form> 
-   
-    <footer id='footer'>
-      Footer
-    </footer>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import { Logout } from './';
 import Images from '../media';
+import './Header.css'
 
 
 const Header = ({ isLoggedIn, setIsLoggedIn, setUser,setToken, setCart, cart,user,token,returnedUserCartId, setReturnedUserCartId,orderPlaced}) => {
   return (
-    <header style={styles.header}>
-      <img className="logo" src={Images.JAAMLOGO} />
-      <nav style={styles.nav}>
-        <NavLink style={styles.link} to="/">
+    <header>
+      <img className='logo' src={Images.JAAMLOGO} />
+      <nav>
+        <NavLink className='link' to="/">
           HOME
         </NavLink>
-        <NavLink style={styles.link} to="/Shop">
+        <NavLink className='link' to="/Shop">
           SHOP
         </NavLink>
-        <NavLink style={styles.link} to="/About">
+        <NavLink className='link' to="/About">
           ABOUT
         </NavLink>
-        <NavLink style={styles.cartLink} to="/Cart">
+        <NavLink className='cartLink' to="/Cart">
         <i className="fa-solid fa-cart-shopping fa-lg"></i>
         </NavLink>
       </nav>
@@ -44,40 +45,6 @@ const Header = ({ isLoggedIn, setIsLoggedIn, setUser,setToken, setCart, cart,use
       </div>
     </header>
   );
-};
-
-const styles = {
-  header: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '.1rem',
-    backgroundColor: '#f2f2f2',
-    fontFamily: 'Arial',
-    width: '100%',
-    overflowX: 'auto',
-  },
-  logo: {
-    fontSize: '2rem',
-    fontWeight: 'bold',
-    color: '#333',
-    textDecoration: 'none',
-    textShadow: '1px 1px 5px #889aa0'
-  },
-  nav: {
-    display: 'flex',
-    gap: '1rem',
-    overflowX: 'auto',
-  },
-  link: {
-    color: '#555',
-    textDecoration: 'none',
-    fontSize: '1.2rem',
-    whiteSpace: 'nowrap',
-  },
-  cartLink: {
-    color: '#889aa0',
-  },
 };
 
 export default Header;
